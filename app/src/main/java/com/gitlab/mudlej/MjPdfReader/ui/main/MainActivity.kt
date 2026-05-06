2875726dc63fec2cf4ad771e3b18f6c88ec4153e
Zixun LI <zli@ogga.fr>
2026-05-06 22:06:12 +0200

    adjust menu

--- app/src/main/java/com/gitlab/mudlej/MjPdfReader/ui/main/MainActivity.kt
+++ app/src/main/java/com/gitlab/mudlej/MjPdfReader/ui/main/MainActivity.kt
@@ -287,7 +287,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateAppTitle() {
-        appTitle.text = pdf.getTitleWithPageNumber()
+        appTitle.text = pdf.getPageCounterText()
     }
 
     private fun initPdfViewAndLoad(viewConfigurator: Configurator, savePassword: Boolean = false) {
@@ -368,7 +368,8 @@ class MainActivity : AppCompatActivity() {
             R.id.printFileOption,
             R.id.searchOption,
             R.id.toggleSecondBarOption,
-            R.id.tocOption
+            R.id.tocOption,
+            R.id.openFileOption2
         )
         barButtonsThatNeedFile.forEach { actionBarMenu.findItem(it)?.isVisible = hasFile }
 
@@ -1152,6 +1153,7 @@ class MainActivity : AppCompatActivity() {
             R.id.reloadOption -> recreate()
             R.id.fullscreenOption -> toggleFullscreen()
             R.id.tocOption -> showBookmarks()
+            R.id.openFileOption2 -> pickFile()
             R.id.switchThemeOption -> switchPdfTheme()
             R.id.openFileOption -> pickFile()
             R.id.copyPageTextOption -> copyPageText(true)
